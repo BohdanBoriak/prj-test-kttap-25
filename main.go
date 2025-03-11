22d541b8f3a5b8816d10b6a11fcc88312909f1b5
BohdanBoriak <lightraven72@gmail.com>
2025-03-11 11:16:33 +0200

    Add: test stage

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -13,7 +15,7 @@ import (
 
 const (
 	totalPoints       = 50
-	pointsPerQuestion = 10
+	pointsPerQuestion = 50
 )
 
 var id uint64 = 1
@@ -21,7 +23,6 @@ var id uint64 = 1
 func main() {
 	fmt.Println("Вітаємо у MathGame!")
 
-	var users []domain.User
 	for {
 		menu()
 
@@ -31,8 +32,11 @@ func main() {
 		switch choice {
 		case "1":
 			user := play()
+			users := getUsers()
 			users = append(users, user)
+			sortAndSave(users)
 		case "2":
+			users := getUsers()
 			for _, u := range users {
 				fmt.Printf(
 					"Id: %v, Name: %s, Time: %v\n",
@@ -110,5 +114,59 @@ func sortAndSave(users []domain.User) {
 	)
 	if err != nil {
 		log.Printf("sortAndSave(os.OpenFile): %s", err)
+		return
 	}
+
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			log.Printf("sortAndSave(file.Close): %s", err)
+		}
+	}()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(users)
+	if err != nil {
+		log.Printf("sortAndSave(encoder.Encode): %s", err)
+	}
+}
+
+func getUsers() []domain.User {
+	var users []domain.User
+
+	file, err := os.Open("users.json")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			_, err = os.Create("users.json")
+			if err != nil {
+				log.Printf("getUsers(os.Create): %s", err)
+			}
+			return nil
+		} else {
+			log.Printf("getUsers(os.Open): %s", err)
+			return nil
+		}
+	}
+
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			log.Printf("getUsers(file.Close): %s", err)
+		}
+	}()
+
+	fInfo, err := os.Stat("users.json")
+	if err != nil {
+		log.Printf("getUsers(os.Stat): %s", err)
+	}
+
+	if fInfo.Size() != 0 {
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&users)
+		if err != nil {
+			log.Printf("getUsers(decoder.Decode): %s", err)
+		}
+	}
+
+	return users
 }
